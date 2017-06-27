05012f52d926bf0c2ef5734cd9529362346771e5
nikgrok <nikita.ostrovsky@gmail.com>
2017-06-27 11:45:10 -0400

    changing to cover all consumer groups

--- files/start.sh
+++ files/start.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-[ -z "${CONSUMER_GROUP}" ] && {
-  echo "Must set CONSUMER_GROUP in env." >&2
-  exit 1
-}
+#[ -z "${CONSUMER_GROUP}" ] && {
+#  echo "Must set CONSUMER_GROUP in env." >&2
+#  exit 1
+#}
 
 BOOTSTRAP_SERVER=${BOOTSTRAP_SERVER:-localhost:9092}
-SLEEP=${SLEEP:-60}
+SLEEP=${SLEEP:-30}
 PREFIX=${PREFIX:-offsets}
 
 GRAPHITE_HOST=${GRAPHITE_HOST:-localhost}
 GRAPHITE_PORT=${GRAPHITE_PORT:-2003}
 
-CMD="kafka-run-class kafka.admin.ConsumerGroupCommand --bootstrap-server ${BOOTSTRAP_SERVER} --describe --group ${CONSUMER_GROUP}"
+#CMD="kafka-run-class kafka.admin.ConsumerGroupCommand --bootstrap-server ${BOOTSTRAP_SERVER} --describe --group ${CONSUMER_GROUP}"
 
 echo "logging env:" >&2
 env >&2
@@ -20,10 +20,15 @@ env >&2
 echo "logging cmd:" >&2
 echo "${CMD}" >&2
 
+CONSUMER_GROUPS=`kafka-run-class kafka.admin.ConsumerGroupCommand --bootstrap-server ${BOOTSTRAP_SERVER} --list`
+
 while :; do
-  ${CMD} | \
-  tail -n+2 | \
-  awk -v prefix="${PREFIX}" -v ts="$(date +%s)" -v cg="${CONSUMER_GROUP}" '{print prefix "." $1 "." cg "." $2 ".lag", $5, ts}' > \
-  /dev/tcp/${GRAPHITE_HOST}/${GRAPHITE_PORT}
+  for CON_GROUP in $CONSUMER_GROUPS
+    CMD="kafka-run-class kafka.admin.ConsumerGroupCommand --bootstrap-server ${BOOTSTRAP_SERVER} --describe --group $CON_GROUP"
+      ${CMD} | \
+      tail -n+2 | \
+      awk -v prefix="${PREFIX}" -v ts="$(date +%s)" -v cg="${CONSUMER_GROUP}" '{print prefix "." $1 "." cg "." $2 ".lag", $5, ts}' > \
+      /dev/tcp/${GRAPHITE_HOST}/${GRAPHITE_PORT}
+  done
   sleep ${SLEEP}
 done
